bdbe284a9dad0cf5768f7c56d29e6fb24df3f170
autumn-brough <78739123+autumn-heights@users.noreply.github.com>
2024-10-29 21:46:59 +0800

    Completed tutorial pt 4

--- polls/urls.py
+++ polls/urls.py
@@ -3,15 +3,27 @@ from django.urls import path
 
 from . import views
 
+# name of app (IMPORTANT for referring to any of the urlconfs by name)
 app_name = "polls"
-urlpatterns = [
-    # ex: /polls/
-    path("", views.index, name="index"),
-    # ex: /polls/5
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results
-    path("<int:question_id>/results", views.results, name="results"),
-    # ex: /polls/5/vote
-    path("<int:question_id>/vote", views.vote, name="vote")
 
+# list of URlconfs
+# Each defines a mapping from a URL pattern to a View function, and also names the urlconf
+# Note that some of the view functions are methods of GenericView classes from over in views.py
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
+# urlpatterns = [
+#     # ex: /polls/
+#     path("", views.index, name="index"),
+#     # ex: /polls/5
+#     path("<int:question_id>/", views.detail, name="detail"),
+#     # ex: /polls/5/results
+#     path("<int:question_id>/results", views.results, name="results"),
+#     # ex: /polls/5/vote
+#     path("<int:question_id>/vote", views.vote, name="vote")
+
+# ]
--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,84 @@
 
-from django.http import Http404, HttpResponse
+from django.db.models import F # django object that refers to a FIELD in the database
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
 
+# Several classes on this page implement generic views
+# The actual view function to be called is their as_view() method
 
-def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}")
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
+    # Tells the ListView that its list will be a list of five Questions
+    def get_queryset(self):
+        # Return the last five published questions
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):    
+    model = Question # Tells the DetailView that its object of interest will be the Question model
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question # Tells the DetailView that its object of interest will be the Question model
+    template_name = "polls/results.html"
+
+
+    
+
+
+# View function for the vote page
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
-
-
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list
-    }
-    return render(request, "polls/index.html", context)
-
-
-
-def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+    try:
+        # request.POST is a dictionary-like object. Try and grab the selected choice attached to it.
+        # If there's no choice, it will throw a KeyError when you try to access it
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Selected invalid choice
+        return render(
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice."
+            }
+        )
+    else:
+        selected_choice.votes = F("votes") + 1 
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        
+
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+
+
+# # View function for the homepage
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     return render(
+#         request,
+#         "polls/index.html",
+#         {
+#             "latest_question_list": latest_question_list
+#         }
+#     )
+
+# # View function for detail page
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+
+
+# # View function for the results page
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
